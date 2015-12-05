eb8a9a6175578ef8774d18b6907ae7d81ac74152
Ronin Lanning <drewLanning@drews-macbook-air.local>
2015-12-04 22:39:58 -0800

    some styling on new habit add screen, started putting date info into AppDelegate so we can know when last launch was and archive data

--- Habit Notebook/AppDelegate.swift
+++ Habit Notebook/AppDelegate.swift
@@ -12,9 +12,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
+  let defaults = NSUserDefaults.standardUserDefaults()
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
+    let launchTime = NSDate()
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateStyle = .NoStyle
+    dateFormatter.timeStyle = .ShortStyle
+    print(dateFormatter.stringFromDate(launchTime))
     return true
   }
 
@@ -26,6 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationDidEnterBackground(application: UIApplication) {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
   }
 
   func applicationWillEnterForeground(application: UIApplication) {
